d503364229e385bf02eab8ef0fa8958562cc13c7
Raj Chauhan <77205464+A00447694@users.noreply.github.com>
2023-01-21 09:14:50 -0400

    challenge 0

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,13 @@ import glob
 import os
 
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,6 +33,16 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    # split data into X and y
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
+    
+    # split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
